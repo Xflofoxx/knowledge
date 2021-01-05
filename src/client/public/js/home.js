07d9a6408de309c6b4a60768890083b8a8cb050d
Dario Olivini <xflofoxx@gmail.com>
2021-01-05 17:38:59 +0100

    reset

--- src/client/public/js/home.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
- * @author Dario Olivini
- * @copyright 2016 Dario Olivini. All rights reserved.
- * See LICENSE file in root directory for full license.
- */
-(function(){
-    
-})();
